00f3adf6f64f139d10dbb0d1df7e63175db4c66c
Johannes Unterstein <junterstein@mesosphere.io>
2017-12-19 19:06:00 +0100

    play with os.Args

--- helloworld/src/go/hello/args.go
+++ helloworld/src/go/hello/args.go
@@ -3,13 +3,25 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	var separator, result string
+	var separator, result string = "", ""
 	for i := 1; i < len(os.Args); i++ {
 		result += separator + os.Args[i]
 		separator = ", "
 	}
 	fmt.Println(result)
+
+	s, sep := "", "" // weird declaration, but ok
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = ", "
+	}
+	fmt.Println(s)
+	fmt.Println(strings.Join(os.Args[1:], ", "))
+	fmt.Println(os.Args[1:])
+	fmt.Println(os.Args[2:])
+	fmt.Println(os.Args[0:])
 }
